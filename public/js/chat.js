435ac47471e7da06922c39b7401302fd3ce06bca
Vanessa Sena <nessa.aline@gmail.com>
2017-12-14 16:37:47 -0300

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -86,8 +86,7 @@ jQuery('#message-form').on('submit', function(e) {
 
   var messageTextBox = jQuery('[name=message]');
 
-  socket.emit('createMessage', {
-    from: 'User',
+  socket.emit('createMessage', {    
     text: messageTextBox.val()
   }, function () {
     messageTextBox.val('')
