2bc5a1c51704823d651b83e6dfcb46493e93745d
Vanessa Sena <nessa.aline@gmail.com>
2017-12-13 21:31:33 -0300

    Add CSS for chat page

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ io.on('connection', (socket) => {
 
   socket.on('createMessage', (newMessage, callback) => {
     io.emit('newMessage', generateMessage(newMessage.from, newMessage.text))
-    callback('This is from the server')
+    callback()
   })
 
   socket.on('createLocationMessage', (coords) => {
