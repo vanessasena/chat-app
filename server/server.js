23fa12b90ff8e95e48ab7a7bba940b7f438298c4
Vanessa Sena <nessa.aline@gmail.com>
2017-12-13 21:09:05 -0300

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
 
-const {generateMessage} = require('./utils/message')
+const {generateMessage, generateLocationMessage} = require('./utils/message')
 
 const publicPath = path.join(__dirname, '../public')
 var port = process.env.PORT || 3000
@@ -24,6 +24,10 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (newMessage, callback) => {
     io.emit('newMessage', generateMessage(newMessage.from, newMessage.text))
     callback('This is from the server')
+  })
+
+  socket.on('createLocationMessage', (coords) => {
+      io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
   })
 
   socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,13 +1,25 @@
 const expect = require('expect')
-const {generateMessage} = require('./message')
+const {generateMessage, generateLocationMessage} = require('./message')
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
-    var from = 'vanessa'
+    var from = 'Vanessa'
     var text = 'This is a message'
 
     var res = generateMessage(from, text)
     expect(res).toMatchObject({from, text})
     expect(typeof res.createdAt).toBe('number')
+  })
+})
+
+describe('generateLocationMessage', () => {
+  it('should genarate correct location object', () => {
+    var from = 'Peter'
+    var latitude = 10
+    var longitude = 45
+    var url = 'https://www.google.com/maps?q=10,45'
+    var message = generateLocationMessage(from, latitude, longitude)
+    expect(message).toMatchObject({from, url})
+    expect(typeof message.createdAt).toBe('number')
   })
 })
